9909c2a1d88581430b81dbca1cb5daac0b55faf9
= <kylerut10@gmail.com>
2020-09-05 17:31:55 -0600

    Data Cleaning Function and Documentation

--- Project1/FirstMachine.py
+++ Project1/FirstMachine.py
@@ -14,11 +14,11 @@ def FirstAlgorithm(file_loc, clean):
   if clean == 1:
     # Remove rows with ?s
     data = RemoveQ(rawdata)
-  elif clean == 2:
-    # Replace ? with most probable answer for that class
-    data = rawdata
   else:
-    data = rawdata
+    # Leave the ? as it's own answer (a for simplicity)
+    # This method is also used if know unkown values
+    # exist for the dataset in question
+    data = rawdata.replace(["?"],"NA")
   
   
   # Training and Testing for 5 x 2 Cross Validation
@@ -30,23 +30,13 @@ def FirstAlgorithm(file_loc, clean):
 
 def RemoveQ(raw):
   # A function which removes ? values
+  #
   # input - raw: Raw data to clean
   # output - data: Cleaned output data
+  
   raw = raw.replace(["?"],"NA")
   data = raw.dropna()
   return data
-  
-def ReplaceAvg(raw):
-  # A function which replaces ? values
-  # with the mean value of that
-  # attribute for that class
-  # input - raw: Raw data to clean
-  # output - data: Cleaned output data
-  return data
-  
-<<<<<<< HEAD
-breastdatastr = 'C:\\Users\\Kyle\\MachineLearning\\Project1\\Data\\breast-cancer-wisconsin.csv'
-=======
+
 breastdatastr = os.path.join('Data', 'breast-cancer-wisconsin.csv')
->>>>>>> 5e4eacf32c1b894f5fefb712083438adee3c43c4
 FirstAlgorithm(breastdatastr, 1)
