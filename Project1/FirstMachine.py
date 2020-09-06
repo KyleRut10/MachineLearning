bb4c7c93840bf2215023abc3864ffc18004405dd
= <kylerut10@gmail.com>
2020-09-05 18:12:50 -0600

    Further removed unused functions

--- Project1/FirstMachine.py
+++ Project1/FirstMachine.py
@@ -18,13 +18,13 @@ def FirstAlgorithm(file_loc, clean = 0):
   # Data Cleaning Station
   if clean == 1:
     # Remove rows with ?s
-    data = RemoveQ(rawdata)
+    raw = rawdata.replace(["?"],"NA")
+    data = raw.dropna()
   else:
     # Leave the ? as it's own answer (a for simplicity)
     # This method is also used if know unkown values
     # exist for the dataset in question
     data = rawdata.replace(["?"],"NA")
-  
   
   # Training and Testing for 5 x 2 Cross Validation
   len_data = len(data)
@@ -33,15 +33,8 @@ def FirstAlgorithm(file_loc, clean = 0):
   
     
 
-def RemoveQ(raw):
-  # A function which removes ? values
-  #
-  # input - raw: Raw data to clean
-  # output - data: Cleaned output data
   
-  raw = raw.replace(["?"],"NA")
-  data = raw.dropna()
-  return data
+  
 
 
 #Function execution
