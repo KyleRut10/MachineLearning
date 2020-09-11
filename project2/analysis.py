5896c22404c39e273704700a650fa8577ec328d5
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-09-11 13:00:39 -0600

    Partial code outline

---
+++ project2/analysis.py
@@ -0,0 +1,72 @@
+import data
+import pandas as pd
+
+
+'''  Implement k-nearest neighbor and be prepared to find the best k value for 
+your experiments. You must tune k and explain in your report how you did the 
+tuning.'''
+def knn(df):
+    # (Num 3)
+    # 
+    # Input -
+    # 
+    # Output - 
+    # 
+    pass
+
+
+''' Implement edited k-nearest neighbor. See above with respect to tuning k. 
+On the regression problems, you should define an error threshold to determine 
+if a prediction is correct or not. This will need to be tuned'''
+def knn_edited(df):
+    # (Num 4)
+    # 
+    # Input -
+    # 
+    # Output - 
+    # 
+    pass
+
+
+''' Implement condensed k-nearest neighbor. See above with respect to tuning 
+k and e'''
+def knn_condenced(df):
+    # (Num 5)
+    # 
+    # Input -
+    # 
+    # Output - 
+    # 
+    pass
+
+
+''' Implement k-means clustering and use the cluster centroids as a reduced 
+data set for k-NN.'''
+def kmeans(df):
+    # (Num 6)
+    # 
+    # Input -
+    # 
+    # Output - 
+    # 
+    pass
+
+
+''' Implement Partitioning Around Medoids for k-medoids clustering and use the 
+medoids as a reduced data set for k-NN. Note that the k for k-medoids is 
+different than the k for k-NN.'''
+def kmediods(df):
+    # (Num 7)
+    # 
+    # Input -
+    # 
+    # Output - 
+    # 
+    pass
+
+
+# TODO: Other steps
+
+if __name__ == '__main__':
+    # put main logic here
+    pass
