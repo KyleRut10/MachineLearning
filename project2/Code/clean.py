387b032a1f091095c7d7b3be0c41888ed77e1ef6
= <kylerut10@gmail.com>
2020-10-03 22:48:53 -0600

    Added On Hot Encoding and Z-Score Standardization

--- project2/Code/clean.py
+++ project2/Code/clean.py
@@ -14,3 +14,19 @@ def na_to_a(data):
 def na_remove(data):
     data = data.replace(["?"],np.nan)
     return data.dropna(axis=0,how='any', inplace=True)
+
+def z_stand(col):
+    # Takes a Raw data Column and standardizes it by its z-score
+    # Input: col - Raw Data Column
+    # Output: Z-score of the Column
+    return (col - col.mean())/col.std
+    
+def on_hot(col):
+    # Takes a column of categorical data and converts it into
+    # a several columns by on_hot encoding.
+    # Input: col - Categorical Data Column
+    # Output: out - Pandas Dataframe of new on-hot columns
+    out = pd.DataFrame()
+    for cat in col.unique():
+        out[cat] = (col == cat)*1
+    return out
