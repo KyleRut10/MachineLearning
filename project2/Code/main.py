74922b4e6c488587c0b4a22ac5f31333db44946f
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-10-05 22:30:12 -0600

    Comments and cry

--- project2/Code/main.py
+++ project2/Code/main.py
@@ -104,6 +104,7 @@ def run_kmeans():
     print('********************')
     print('--------------------')
     # glass dataset
+    '''
     print('***GLASS DATASET****')
     tune_run_kmeans(d.type_glass, d.data_glass)
 
@@ -114,7 +115,11 @@ def run_kmeans():
     # Segmentation dataset
     print('\n***SEGMENTATION***')
     tune_run_kmeans(d.type_segmentation, d.data_segmentation)
+    '''
 
+    # abalone dataset
+    print('\n***ABALONE***')
+    tune_run_kmeans(d.type_abalone, d.data_abalone)
 
 def demo():
     print('Get k-means dataset')
