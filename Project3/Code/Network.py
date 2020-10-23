12d8165f7c7232eeb4b240c542e8ab43b213be71
= <kylerut10@gmail.com>
2020-10-23 16:56:34 -0600

    More Comments

--- Project3/Code/Network.py
+++ Project3/Code/Network.py
@@ -1,6 +1,6 @@
 
 
-def network(inputs, num_layers, num_node, num_out, mode):
+def network(inputs, num_layers, num_node, num_out, mode, momentum):
     # A multi-layer feedforward network with backpropogation.
     # Inputs -
     #     inputs: Arbitrary number of inputs to the algorithm
@@ -10,13 +10,20 @@ def network(inputs, num_layers, num_node, num_out, mode):
     #     num_out: Arbitrary number of outputs from the algorithm
     #     mode: Whether the function is using performing regression
     #         or classification on the data.
-    # Output(s?) -
-    #     
+    #     momentum: Whether or not/How much momentum should be
+    #         used for the algorithm
+    # Outputs -
+    #     conv_rate: Convergence Rate of the algorithm
+    #     F1_score: F1 score for the algorithm
     
     # Calculate Number of Inputs from inputs
+    
+    
     if(mode == "r"):
+        # The first column of inputs should be the target
         return regression(inputs, num_layers, num_node, num_out)
     else if(mode == "c"):
+        # The first row of inputs should be the class
         return classification(inputs, num_layers, num_node, num_out)
     
 def regression(inputs, num_layers, num_node, num_out):
@@ -24,4 +31,10 @@ def regression(inputs, num_layers, num_node, num_out):
     # See network for inputs and outputs
     
     # Feedforward - An input pattern is applied to the input layer
-    #     and it propogates layer by layer
+    #     and it propogates layer by layer until an output is produced.
+    #     Here this means we calculate an error term for the difference
+    #     between the expected quantitative output and our output.
+    
+    # Backpropogation - Once the error
+    
+    # Don't forget momentum
---
+++ Project3/Code/Tuning.py
@@ -0,0 +1,22 @@
+
+
+
+def tune(inputs, num_layers, num_out, mode, momentum):
+    # A function which outputs the number of nodes per hidden
+    #     layer for the Backpropagation neural network
+    # Inputs -
+    #     inputs: Arbitrary number of inputs to the algorithm
+    #     num_layers: Number of Hidden Layers
+    #     num_out: Arbitrary number of outputs from the algorithm
+    #     mode: Whether the function is using performing regression
+    #         or classification on the data.
+    #     momentum: Whether or not/How much momentum should be
+    #         used for the algorithm
+    # Output -
+    #     num_node: An x by num_layers array containing the
+    #         number of hidden nodes per layer
+    
+    # Calculate num_inputs from inputs
+    
+    for i in range(0:num_inputs):
+        
