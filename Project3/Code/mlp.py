9ed1333f90b8c190e49fd3aae59ddbaf0a25878e
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-10-25 20:24:21 -0600

    I have no idea whay on Earth I'm doing...

--- Project3/Code/mlp.py
+++ Project3/Code/mlp.py
@@ -4,13 +4,16 @@ import numpy as np
 
 
 class MLP:
-   # Class Variables
-   # num_hidden: Number hidden layers
-   # hidden_nodes: List of how many hidden nodes there are per layer
-   # num_outputs: Number output nodes
-   # train: df of training data
-   # test: df of testing data
-
+    # Class Variables
+    # num_hidden: Number hidden layers
+    # hidden_nodes: List of how many hidden nodes there are per layer
+    # num_outputs: Number output nodes
+    # train: df of training data
+    # test: df of testing data
+        
+    linespace = np.linspace(-100, 100, 200) 
+    sig = 1/(1 + np.exp(-linespace)) 
+    
     def __init__(self, hidden_nodes, num_outputs, training, test, mode):
         self.num_hidden = len(hidden_nodes)
         self.hidden_nodes = hidden_nodes
@@ -41,13 +44,21 @@ class MLP:
             # Reshape into 2D Numpy array
             self.weights.append(np.array(hw).reshape(h, w))
 
-            print(self.weights[-1])
+            #print(self.weights[-1])
 
 
         # TODO: Figure out how this is going to work
         converge = False
         while not converge:
             for i,pt in self.training.iterrows():
+                activations = []
+                # feedforward computation
+                # a: activation function values
+                
+                
+                
+
+
                 #print(pt)
                 pass
 
@@ -55,9 +66,18 @@ class MLP:
             # self variable when do final updates
 
             break
-    def forward(self):
+    def forward(self, W, a0):
+        #z = np.matmult(W,a)
+        #print(z)
+        #print(1/(1+np.exp(z))
         pass
-
+    
+    def activation(self, z):
+        a = []
+        for zz in z:
+            a.append(1/(1+np.exp(z)))
+        return np.array(a)
+    
     def backward():
         pass
 
