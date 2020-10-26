b35c95536057557a1ed96f8c7257d78d31fdf0cf
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-10-25 18:24:37 -0600

    Init idea of what I think we'll need

---
+++ Project3/Code/mlp.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+
+
+class mlp:
+   # Class Variables
+   # num_hidden: Number hidden layers
+   # hidden_nodes: List of how many hidden nodes there are per layer
+   # num_outputs: Number output nodes
+   # train: df of training data
+   # test: df of testing data
+
+    def __init__(self, num_hidden, hidden_nodes, num_outputs, train, test):
+        self.num_hidden = num_hidden
+        self.hidden_nodes = hidden_nodes
+        self.num_outputs = num_outputs
+        self.train = train
+        self.test = test
+
+
+
+    def train():
+        pass
+
+    def forward():
+        pass
+
+    def backward():
+        pass
+
+    def run():
+        # Run one example through trained network
+        pass
