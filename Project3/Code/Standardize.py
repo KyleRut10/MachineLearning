6a765dfc8210d62162a21fa7fcf2b18e640a8fd9
= <kylerut10@gmail.com>
2020-10-28 15:08:32 -0600

    Categorical

--- Project3/Code/Standardize.py
+++ Project3/Code/Standardize.py
@@ -4,3 +4,13 @@ def standardize(column):
     # Standardizes a column by z-score normalization
     
     return column - mean(column)/column.std
+
+def on_hot(col):
+    # Takes a column of categorical data and converts it into
+    # a several columns by on_hot encoding.
+    # Input: col - Categorical Data Column
+    # Output: out - Pandas Dataframe of new on-hot columns
+    out = pd.DataFrame()
+    for cat in col.unique():
+        out[cat] = (col == cat)*1
+    return out
