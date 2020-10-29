89572e02360681402903def8bcefd3a2a87b6829
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-10-29 14:44:20 -0600

    Removed unused files

--- Project3/Code/Clean.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def clean(rawdata):
-    # A function to remove rows with question marks from the data
-    
-    raw = rawdata.replace(["?"],np.nan)
-    data = raw.dropna(axis=0,how='any')
-    
-    return data
