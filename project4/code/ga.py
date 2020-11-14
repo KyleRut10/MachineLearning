02eec991142a757ce8a0d2e1ae88cec74a8eef99
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-11-14 11:25:11 -0700

    Initilize population

--- project4/code/ga.py
+++ project4/code/ga.py
@@ -8,5 +8,31 @@ class GA(NN):
                        pkl_file)
 
 
-    def train():
-       pass 
+    def train(self, pc, pm, num_chrom):
+        # Train using the genetic algorithm
+        # inputs
+        # pc - Probability of crossover occuring
+        # pm - probability of mutation occuring
+        # num_chrom - number of individuals in the population
+
+        population = []  # empty array to hold individuals in population
+        # initilize population of size num_chrom
+        for i in range(num_chrom):
+            # get random weights and turn into chromosome then add to pop
+            weights = self.initilize_weights()
+            chromosome = self.weights_to_chromosome(weights)
+            population.append(chromosome)
+
+        # selection
+
+        # crossover and mutation
+
+        # replacement
+
+
+
+    def weights_to_chromosome(self, weights):
+        # this method will convert the weight matrix for the nerual network
+        # into a linear chormosome
+
+        pass
