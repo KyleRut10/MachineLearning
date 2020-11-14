d710f6413fcdd5d2a83c8432f3e6d15b03d9674f
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-11-14 11:05:00 -0700

    Inherit from NN class and add basic outline

--- project4/code/ga.py
+++ project4/code/ga.py
@@ -1,1 +1,12 @@
 # Inherit from base class and do magical things :)
+from nn import NN
+
+class GA(NN):
+    def __init__(self, hidden_nodes='', mode='', training='', testing='',
+        num_outputs='', pkl_file=''):
+        super.__init__(hidden_nodes, mode, tarining, testing, num_outputs,
+                       pkl_file)
+
+
+    def train():
+       pass 
